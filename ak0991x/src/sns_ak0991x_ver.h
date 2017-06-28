2e4764616bd5de356b22dcd28e9baf7acaad0e43
Tomoya NAKAJIMA <nakajima.tcd@om.asahi-kasei.co.jp>
2017-06-28 17:52:14 +0900

    Add sns_ak0991x_ver.h for release.

---
+++ ak0991x/src/sns_ak0991x_ver.h
@@ -0,0 +1,35 @@
+#pragma once
+/**
+ * @file sns_ak0991x_ver.h
+ *
+ * Driver version
+ *
+ * Copyright (c) 2017 Asahi Kasei Microdevices
+ * All Rights Reserved.
+ * Confidential and Proprietary - Asahi Kasei Microdevices
+ *
+ * Copyright (c) 2017 Qualcomm Technologies, Inc.
+ * All Rights Reserved.
+ * Confidential and Proprietary - Qualcomm Technologies, Inc.
+ **/
+
+/**
+ * EDIT HISTORY FOR FILE
+ *
+ * This section contains comments describing changes made to the module.
+ * Notice that changes are listed in reverse chronological order.
+ *
+ *
+ * when         version    who              what
+ * --------     --------   ----------       ---------------------------------
+ * 06/13/17     010003     Qualcomm         Work on OpenSSC v5.0.4.
+ * 05/11/17     010002     AKM              Add AK09917D support.
+ * 05/11/17                AKM              Add island mode support.
+ * 05/11/17                AKM              Add DAE sensor support.
+ * 04/04/17     010001     AKM              Fix IRQ configuration.
+ * 04/04/17                AKM              Fix ODR attribute configuration.
+ * 02/20/17     010000     AKM              First version.
+ *
+ **/
+
+#define AK0991X_DRIVER_VERSION 10003  // major:01 minor:00 revision:03
