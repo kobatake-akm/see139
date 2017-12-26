13077d1b301b561b79bbc0c43c6f0908cb34ead5
Unknown <mfukasawa@akm.com>
2017-12-26 09:46:04 -0800

    Fixed comparison of the heart beat timer

--- ak0991x/src/sns_ak0991x_ver.h
+++ ak0991x/src/sns_ak0991x_ver.h
@@ -23,6 +23,7 @@
  * when         version    who              what
  * --------     --------   ----------       ---------------------------------
  * 12/25/17     010050     AKM              Modified for DC-Lib.
+ * 12/25/17                AKM              Fixed comparison of the timestamp for heart_beat_timer_event.
  * 12/20/17     010049     Qualcomm/AKM     Merged the version 010048 and dual sensor modification
  * 12/18/17                Qualcomm/AKM     Fixed dual sensor for simultaneous streaming.
  * 12/11/17                AKM              Debugged for sending SNS_STD_MSGID_SNS_STD_FLUSH_EVENT
