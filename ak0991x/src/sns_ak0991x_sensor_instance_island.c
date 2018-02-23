b496dd520e2be42361a02fc33f7267be7ed676b7
Unknown <mfukasawa@akm.com>
2018-02-23 11:55:49 -0800

    removed "Execute handle timer.." comments on polling mode.

--- ak0991x/src/sns_ak0991x_sensor_instance_island.c
+++ ak0991x/src/sns_ak0991x_sensor_instance_island.c
@@ -380,9 +380,9 @@ static sns_rc ak0991x_inst_notify_event(sns_sensor_instance *const this)
           if (!state->mag_info.use_dri && state->reg_event_done)
           {
             state->force_fifo_read_till_wm = true;
-            AK0991X_INST_PRINT(LOW, this, "Execute handle timer event. now %u req_timeout_time %u",
-                       (uint32_t)now,
-                       (uint32_t)state->system_time);
+//            AK0991X_INST_PRINT(LOW, this, "Execute handle timer event. now %u req_timeout_time %u",
+//                       (uint32_t)now,
+//                       (uint32_t)state->system_time);
 
             // check DRDY status.
             ak0991x_get_st1_status(this);
