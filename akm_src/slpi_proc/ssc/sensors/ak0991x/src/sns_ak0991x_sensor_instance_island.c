14a6978abcce51b8281a517f7afb722672119ff3
mfukasawa <mfukasawa@akm.com>
2019-02-21 14:39:02 -0800

    Qualcomm reviewed version for Ver2.00.43.

--- akm_src/slpi_proc/ssc/sensors/ak0991x/src/sns_ak0991x_sensor_instance_island.c
+++ akm_src/slpi_proc/ssc/sensors/ak0991x/src/sns_ak0991x_sensor_instance_island.c
@@ -3,7 +3,7 @@
  *
  * AK0991X Mag virtual Sensor Instance implementation.
  *
- * Copyright (c) 2016-2018 Asahi Kasei Microdevices
+ * Copyright (c) 2016-2019 Asahi Kasei Microdevices
  * All Rights Reserved.
  *
  * Copyright (c) 2016-2018 Qualcomm Technologies, Inc.
