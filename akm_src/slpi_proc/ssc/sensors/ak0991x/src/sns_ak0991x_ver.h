f6cbcf01c56c8000151604c5a4e6f7027d931d1c
MASANORI IBARAKI <ibaraki.mb@om.asahi-kasei.co.jp>
2019-11-14 20:56:21 +0900

    Fixed for new QAWA(1.0.78.0) failure

--- akm_src/slpi_proc/ssc/sensors/ak0991x/src/sns_ak0991x_ver.h
+++ akm_src/slpi_proc/ssc/sensors/ak0991x/src/sns_ak0991x_ver.h
@@ -22,6 +22,7 @@
  *
  * when         version    who              what
  * --------     --------   ----------       ---------------------------------
+ * 11/14/19                AKM              Fixed for new QAWA(1.0.78.0) failure about timestamp of physical_config_events
  * 11/01/19     026207     AKM              Fixed JIRA2458 Re-evaluate a configuration when two request are made
  * 10/23/19     026206     AKM              Fixed JIRA2467 Changed a calculation method of chosen_report_rate
  * 10/18/19                Qualcomm         Fixed JIRA-2547 - Increased buffer length for encoding timer request
